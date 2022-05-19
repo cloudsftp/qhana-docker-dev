abe0ee7a13f20a249f19459f7821ab229bf36398
Fabian Weik <fabian.weik@protonmail.com>
2022-05-19 10:25:38 +0200

    check for missing npm, ng

--- start.sh
+++ start.sh
@@ -2,7 +2,17 @@
 
 # Start UI first
 cd qhana-ui
+if ! [ -x "$(command -v npm)" ]; then
+    echo "Please install npm first!"
+    exit 2
+fi
 npm install
+
+if ! [ -x "$(command -v ng)" ]; then
+    echo "Please install @angular/cli gobally"
+    echo "You can do this by running 'sudo npm i -g @angular/cli'"
+    exit 2
+fi
 ng serve &
 cd -
 
