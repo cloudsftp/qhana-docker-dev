e0ee3cf0a69583a5ca78226f0173f64b5fc8a181
Fabian Weik <fabian.weik@protonmail.com>
2022-05-29 21:30:40 +0200

    add option to rebuild the plugin runner

--- start.sh
+++ start.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+REBUILD_IMAGES="false"
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --rebuild-runner | -r)
+            REBUILD_IMAGES="true"
+            IMAGES_TO_REBUILD="${IMAGES_TO_REBUILD} qhana-plugin-runner"
+            shift
+            ;;
+        *)
+            echo "Usage: $0 [OPTIONS]"
+            echo
+            echo "OPTIONS:"
+            echo
+            echo "   --rebuild-runner       Rebuilds qhana-plugin-runner"
+            echo
+            
+            exit 2
+    esac
+done
+
+# Rebuild images
+[ "${REBUILD_IMAGES}" = "true" ] && docker-compose build --parallel ${IMAGES_TO_REBUILD}
+
 # Start UI first
 cd qhana-ui
 if ! [ -x "$(command -v npm)" ]; then
@@ -21,4 +45,4 @@ cd -
 docker-compose --profile with_db up
 
 # Stop ng
-kill "$NG_PID"
+[ -d "/proc/${NG_PID}" ] && kill "${NG_PID}"
