727d09c05c1d492ea07eb93823e08727f2154fd7
Fabian Weik <fabian.weik@protonmail.com>
2022-06-12 11:48:50 +0200

    start plugin runner and worker outside of docker

--- start.sh
+++ start.sh
@@ -49,8 +49,24 @@ ng serve --poll 2000 &
 NG_PID=$!
 cd -
 
+# Then start the plugin runner
+cd qhana-plugin-runner
+if ! [ -x "$(command -v poetry)" ]; then
+    echo "Please install poetry first!"
+    exit 2
+fi
+poetry run flask run &
+PLUGIN_RUNNER_PID=$!
+
+poetry run invoke start-broker
+poetry run invoke worker &
+WORKER_PID=$!
+cd -
+
 # Start the rest with docker compose
 docker-compose --profile with_db up
 
-# Stop ng
+# Stop ng and flask
 [ -d "/proc/${NG_PID}" ] && kill "${NG_PID}"
+[ -d "/proc/${PLUGIN_RUNNER_PID}" ] && kill "${PLUGIN_RUNNER_PID}"
+[ -d "/proc/${WORKER_PID}" ] && kill "${WORKER_PID}"
