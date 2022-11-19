26ac65bfd7d5b3c0a8a75704871e57647251cf1d
Fabian Weik <fabian.weik@protonmail.com>
2022-11-19 17:12:58 +0100

    add option to import WIP

--- data.sh
+++ data.sh
@@ -6,9 +6,15 @@ declare -A to_store=(
     [qhana-plugin-runner]=app/instance
 )
 
-# docker run --rm --volumes-from qhana-docker-dev-qiskit-service-1 -v $(pwd)/Backup:/backup ubuntu cp data/app.db backup
+declare -a containers=(
+    redis
+    postgres
+)
 
 export() {
+    mkdir -p Backup
+    rm -rf Backup/*
+
     for container_name in "${!to_store[@]}"
     do
         docker run --rm \
@@ -16,6 +22,26 @@ export() {
             -v $(pwd)/Backup:/backup \
             ubuntu tar cfz /backup/${container_name}.tar.gz ${to_store[$container_name]}
     done
+    
+    for container in "${containers}"
+    do
+        docker export -o Backup/${container}.tar qhana-docker-dev-${container}-1
+    done
 }
 
-export
+import() {
+    for container_name in "${!to_store[@]}"
+    do
+        docker run --rm \
+            --volumes-from qhana-docker-dev-${container_name}-1 \
+            -v $(pwd)/Backup:/backup \
+            ubuntu bash -c "tar xfz /backup/${container_name}.tar.gz"
+    done
+    
+    for container in "${containers}"
+    do
+        docker import Backup/${container}.tar
+    done
+}
+
+$1
