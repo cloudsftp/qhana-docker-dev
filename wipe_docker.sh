9744fd64252169988665db0f41768649b46ee7b9
Fabian Weik <fabian.weik@protonmail.com>
2022-11-20 15:07:19 +0100

    update wipe docker script

--- wipe_docker.sh
+++ wipe_docker.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-CONTAINERS="$(docker ps -qa)"
-[ -n "$CONTAINERS" ] && docker stop $CONTAINERS
-[ -n "$CONTAINERS" ] && docker rm --force $CONTAINERS
-
-IMAGES="$(docker image ls -qa)"
-[ -n "$IMAGES" ] && docker rmi --force $IMAGES
-
-VOLUMES="$(docker volume ls -q)"
-[ -n "$VOLUMES" ] && docker volume rm --force $VOLUMES
+docker system prune -a -f
